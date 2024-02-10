fb9cd86e0500b23b50dfc34abee7137bdd65db2e
Grimille <mickael.dhainaut@gmail.com>
2024-02-10 17:50:43 +0100

    ZWIP-2 - First discord authentication

--- index.ts
+++ index.ts
@@ -1,1 +1,13 @@
+import { Client, GatewayIntentBits, Events } from "discord.js";
+
 console.log("Hello via Bun!");
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.on(Events.ClientReady, () => {
+  console.log("I am ready!");
+});
+
+client.login("MzIxNzEyOTI5NTg4Mzc5Njc5.Gq55_4.xNHfSd_c0QyzfOTjUrET9eGER93D42l3Tlctz8");
+
+//MzIxNzEyOTI5NTg4Mzc5Njc5.Gq55_4.xNHfSd_c0QyzfOTjUrET9eGER93D42l3Tlctz8
