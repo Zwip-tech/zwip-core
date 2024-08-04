606f0b1fbebb268e7f6ad32a5f44f0f78c3b9271
Grimille <mickael.dhainaut@gmail.com>
2024-08-04 22:43:50 +0200

    ZWIP-26 - Remove isSlashCommand (dead code)

--- src/core/commands/CommandMetadata.ts
+++ src/core/commands/CommandMetadata.ts
@@ -3,6 +3,5 @@ import { CommandBase } from "./CommandBase";
 export interface CommandMetadata {
   label: string;
   aliases: string[];
-  isSlashCommand: boolean;
   target: new (label: string, aliases: string[]) => CommandBase;
 }
